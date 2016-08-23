1b0b383ab2bdb1466a8df6a59319aba64ffa19e6
Jonas Wiesel <wieseljonas@gmail.com>
2016-08-23 14:09:41 +0200

    finished algorithm

--- scraper.js
+++ scraper.js
@@ -1,4 +1,6 @@
 var Promise = require("bluebird");
+var sslRootCAs = require('ssl-root-cas/latest')
+sslRootCAs.inject()
 var request = require("request");
 var cheerio = require('cheerio');
 var json2xls = require('json2xls');
@@ -11,22 +13,24 @@ getUrls()
 
 
 function getUrls(){
-	var promises = [];
+	 var characters = ["1","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 	var pageUrls = [];
 	var urls = [];
-	for (var i = 1; i < 39; i++) {
-		pageUrls.push('http://startupticker.ch/en/topics?category=Financing&page=' + i)
+	for (c of characters) {
+		pageUrls.push('https://www.swissmadesoftware.org/en/home/companies~'+ c +'~.html');
 	}
 	Promise.map(pageUrls, pageUrl => new Promise((resolve, reject) => {
 	    console.log('Visiting Page ' + pageUrl);
-	    request(pageUrl, function (error, response, html) {
+	    request({"rejectUnauthorized": false,
+		    "url": pageUrl,
+		    "method": "GET"
+		    }, function (error, response, html) {
 		  	if (!error && response.statusCode == 200) {
 		    	var $ = cheerio.load(html,{
 					ignoreWhitespace: true
 				});
-
-		    	$('div.topic').each(function(i, element){
-		    		var url = 'http://startupticker.ch' + $(this).children('div.article').children('h2').children('a').attr('href')
+		    	$('ul.company-list li').each(function(i, element){
+		    		var url = 'https://www.swissmadesoftware.org' + $(this).children('li a').attr('href')
 		      		urls.push(url)
 		   	 	});
 		   	 	resolve();
@@ -42,59 +46,50 @@ function getUrls(){
 	});
 }
 
-
 function parsePages(urls) {
-	var parsedPages = [];
+	var parsedCompany = [];
 	Promise.map(urls, url => new Promise((resolve, reject) => {
 	    console.log('Visiting page ' + url);
-	    request(url, function (error, response, html) {
+	    request({"rejectUnauthorized": false,
+		    "url": url,
+		    "method": "GET"
+		    }, function (error, response, html) {
 		  	if (!error && response.statusCode == 200) {
 		    	var $ = cheerio.load(html,{
-						    ignoreWhitespace: true
+						    ignoreWhitespace: false
 						});
-				var title = $('div.article h2').text()
-				var date = $('span.list_date').text().slice(0,10)
-				var content = $('div.content').text()
-				var intro = $('div.intro').text()
-				var companyUrl = 'http://startupticker.ch' + $('div.company a').attr('href')
-				var logoUrl = $('div.company a img').attr('src')
-
-				var article = {
-					date: date,
-					logoUrl: logoUrl,
+		    	
+				var companyName = $('h1.effect-title').text()
+				var companyUrl = $('figure.image a').attr('href')
+				var description = $('div.text div.cell-large-left p').first().text().replace(/\s+/g, ' ')
+				var contacts = $('div.text div.cell-large-left p').eq(1).text().replace(/\s+/g, ' ')
+				var address = $('div.text div.cell-large-left p').eq(2).text().replace(/\s+/g, ' ').split(" ").filter(function (e) { return  e != ''})
+				var other = $('div.text div.cell-large-left p').last().text().split("\n")
+				var logoUrl = 'https://www.swissmadesoftware.org' +$('figure.image a img').attr('src')
+				var company = {
+					companyName: companyName,
 					companyUrl: companyUrl,
-					title: title,
-					url: url,
-					intro: intro,
-					content: content,
+					description: description,
+					contacts: contacts,
+					email: other.filter(function (e) { return  e.indexOf("@") !== -1}).join(";").replace(/\s+/g, ' '),
+					telephone: other.filter(function (e) { return  e.indexOf("Tel") !== -1}).join(";").replace(/\s+/g, ' '),
+					fax: other.filter(function (e) { return  e.indexOf("Fax") !== -1}).join(";").replace(/\s+/g, ' '),
+					street: address.splice(0, address.length -3).join(" "),
+					postCode: address[address.length -3],
+					canton: address[address.length -2],
+					country: address[address.length -1],
+					logoUrl: logoUrl,					
 				};
-				request(article.companyUrl, function (error, response, html) {
-				  	if (!error && response.statusCode == 200) {
-				    	var $ = cheerio.load(html,{
-								    ignoreWhitespace: true
-								});
-						var companyName = $('div.cms-page h2').first().text()
-						var details = $('div.inline div')
-						var canton = details.first().text().replace('Canton:','');
-						var foundation = details.last().text().replace('Founded:','');
-
-						article["companyName"] = companyName
-						article["canton"] = canton
-						article["foundation"] = foundation
-						parsedPages.push(article)
-				   	 	resolve();
-					}
-					resolve()
-				});
+				parsedCompany.push(company)
+				resolve()
 			}
 		})
 	}), {
 	    concurrency: 4
 	}).then(() => {
-	    console.log(parsedPages);
 	    console.log('All Pages parsed Created!');
 
-		var xls = json2xls(parsedPages);
+		var xls = json2xls(parsedCompany);
 
 		fs.writeFileSync('data.xlsx', xls, 'binary');
 	}).catch(err => {
